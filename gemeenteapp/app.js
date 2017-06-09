75acff80b6ddeb04ab69f6d3b78c584af8787603
Mike <mike@ws-165-139.han.nl>
2017-06-09 12:45:35 +0200

    tijdelijke commit, taak object toegevoegd, /takentest toegevoegd

--- gemeenteapp/app.js
+++ gemeenteapp/app.js
@@ -16,6 +16,7 @@ var streamHome = require('./routes/streamsHome');
 var streamCreate = require('./routes/createStream');
 var createWallet = require('./routes/createWallet');
 var transaction = require('./routes/transaction');
+var takentest = require('./routes/takentest');
 
 var app = express();
 
@@ -42,6 +43,7 @@ app.use('/streamHome',streamHome);
 app.use('/createStream', streamCreate);
 app.use('/createWallet', createWallet);
 app.use('/transaction', transaction);
+app.use('/takentest', takentest);
 
 
 // catch 404 and forward to error handler
