ffda9903ba9eb353b632fd6bf35bf9355702b9da
Turbotondel <tim.dekruijf1998@gmail.com>
2017-06-09 10:11:59 +0200

    Finished streamDetails

--- gemeenteapp/routes/streamDetails.js
+++ gemeenteapp/routes/streamDetails.js
@@ -10,16 +10,33 @@ var multichain = require("multichain-node")({
 router.get('/', function(req, res, next) {
     multichain.listStreamPublishers({stream: req.query.name, verbose: false, count: 10, start: -10}, (err, publisher)=> {
         if(err){
+            console.log(err);
             throw err;
         }
         multichain.listStreams({stream: req.query.name, verbose: false, count: 10, start: -10}, (err, stream) => {
             if(err) {
+                console.log(err);
                 throw err;
             }
             multichain.listStreamItems({stream: req.query.name, verbose: false, count: 10, start: -10}, (err, items) => {
                 if(err) {
+                    console.log(err);
                     throw err;
-                }                
+                }
+
+                /*for(var item in items.data) {
+                    console.log(items.data);
+                    var output = new Buffer(item, 'hex');
+                    console.log(item);
+                }*/
+
+                for (var index = 0; index < items.length; index++) {
+                    console.log(items[index].data);
+                    var output = new Buffer(items[index].data, 'hex');
+                    items[index].data = output.toString();
+                    console.log(items[index].data);                    
+                }
+
                 res.render('streamDetails', {
                     streamPublisher: publisher,
                     title: 'Details', 
@@ -31,5 +48,7 @@ router.get('/', function(req, res, next) {
         })
     })
 });
+
+
 
 module.exports=router
