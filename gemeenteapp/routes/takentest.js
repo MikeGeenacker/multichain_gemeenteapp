c6609e6860144b5b592ab9faa8e8a043458e88b8
Bas <bbaspolman@gmail.com>
2017-06-12 11:45:41 +0200

    Changed takentest

--- gemeenteapp/routes/takentest.js
+++ gemeenteapp/routes/takentest.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var taak = require('../objects/taak')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var t = new taak();
-	t.test();
-	t.get('testtaakstream3',function(taken) {
-		res.render('taken', taken);
+  var linkje = req.query.name;
+	t.get(linkje, function(taken) {
+		var takenLengte = taken.length - 1;
+		res.render('taakDetails', {opdrachten: taken[2], huidigeTaak: linkje});
 	});
 });
 
