e03631eacb97c9f914a6b019f3d318c57e7e0df3
Turbotondel <tim.dekruijf1998@gmail.com>
2017-06-12 14:50:26 +0200

    Made view wallets and wallet details with bootstrap

---
+++ gemeenteapp/routes/wallets.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var multichain = require("multichain-node")({
+    port: 7348,
+    host: '136.144.155.184',
+    user: "multichainrpc",
+    pass: "uPc6civWyGanmrmAgn3Tn9pPgYR3noMQt9nPMun9GeD"
+});
+
+
+/*GET stream names*/
+router.get('/', function(req, res, next) {
+    multichain.listAddresses((err, addresses) => {
+        if(err) {
+            throw err;
+        }
+        res.render('wallets', {title: 'Streams', wallets: addresses});
+    })
+});
+
+module.exports = router;
